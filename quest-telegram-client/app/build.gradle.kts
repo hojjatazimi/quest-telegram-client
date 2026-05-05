b2f1ac7f0c8f3ab81d5d747b17caf90d759c9252
Hojjat Azimi <azimi.hojjat@yahoo.com>
2026-05-05 12:33:18 -0600

    fixed building issues

--- quest-telegram-client/app/build.gradle.kts
+++ quest-telegram-client/app/build.gradle.kts
@@ -54,6 +54,11 @@ android {
         }
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
         jvmTarget = "17"
     }
